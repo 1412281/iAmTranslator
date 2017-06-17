eb5bc31d5440bef292a4398e5892ed7aaedb38e4
lamtran2601 <lamtran2601@gmail.com>
2017-06-17 22:48:01 +0700

    fix dict table view

---
+++ Translator/Dictionary/DictionaryTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  DictionaryTableViewCell.swift
+//  Translator
+//
+//  Created by LamTran on 6/17/17.
+//  Copyright © 2017 LamTran. All rights reserved.
+//
+
+import UIKit
+
+class DictionaryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var key: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
