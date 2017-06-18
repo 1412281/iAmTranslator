377529c93ff95de07fca89c4239e745d30a3c0ed
lamtran2601 <lamtran2601@gmail.com>
2017-06-18 10:21:45 +0700

    fix model, db

---
+++ Translator/ViewController/PlayTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PlayTableViewCell.swift
+//  Translator
+//
+//  Created by LamTran on 6/18/17.
+//  Copyright © 2017 LamTran. All rights reserved.
+//
+
+import UIKit
+
+class PlayTableViewCell: UITableViewCell {
+
+ 
+    @IBOutlet weak var label: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
