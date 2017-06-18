859b37d56c9bd0d2aa3e5055f86c90c93cffae56
malartsoft <hoanglam@Trans-MacBook-Pro.local>
2017-06-19 02:12:38 +0700

    Finish Login

--- Translator/LoginViewController.swift
+++ Translator/LoginViewController.swift
@@ -7,23 +7,29 @@
 //
 
 import UIKit
-
+import Firebase
 class LoginViewController: UIViewController {
     
     
+    @IBOutlet weak var email: CustomLoginTextField!
+    @IBOutlet weak var pass: CustomLoginTextField!
+    @IBOutlet weak var error: UILabel!
+    
+    @IBAction func login(_ sender: Any) {
+        LogIn()
+    }
     @IBAction func createAccount(_ sender: Any) {
         
-        let storyboard=UIStoryboard(name:"Play", bundle: nil)
+        let storyboard=UIStoryboard(name:"Main", bundle: nil)
         let vc=storyboard.instantiateViewController(withIdentifier: "register") as! RegisterViewController
         self.present(vc, animated: true, completion: nil)
         
     }
    
-    @IBOutlet weak var email: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        error.isHidden=true
         
                
         // Do any additional setup after loading the view.
@@ -34,15 +40,33 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func LogIn(){
+        if (pass.text?.characters.count)! < 7 {
+            error.isHidden=false;
+            self.error.text="Password must be greater than 6 characters"
+            return
+        }
+        else{
+            error.isHidden=true;
+        }
 
-    /*
-    // MARK: - Navigation
+        Auth.auth().signIn(withEmail: email.text!, password: pass.text!, completion: {
+            user,error in
+            if(error != nil){
+                self.error.isHidden=false;
+                self.error.text="Input email was wrong"
+                return
+            }
+            else{
+                let storyboard=UIStoryboard(name:"Play", bundle: nil)
+                let vc=storyboard.instantiateViewController(withIdentifier: "playNavi") as UIViewController
+                self.present(vc, animated: true, completion: nil)
+                return
+            }
+        })
+        //error.isHidden=false;
+       // self.error.text="Email's format was wrong"
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
 
 }
--- Translator/RegisterViewController.swift
+++ Translator/RegisterViewController.swift
@@ -7,22 +7,36 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
     
     
+    
+    @IBOutlet weak var email: CustomLoginTextField!
+    
+    @IBOutlet weak var pass: CustomLoginTextField!
+    @IBOutlet weak var repass: CustomLoginTextField!
+    @IBOutlet weak var error: UILabel!
+    
+    @IBAction func registerButton(_ sender: Any) {
+        Register()
+    }
+    
     @IBAction func backRegister(_ sender: Any) {
         
-        let storyboard=UIStoryboard(name:"Play", bundle: nil)
+        let storyboard=UIStoryboard(name:"Main", bundle: nil)
         let vc=storyboard.instantiateViewController(withIdentifier: "login") as! LoginViewController
         self.present(vc, animated: true, completion: nil)
+        
         
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        error.isHidden=true;
         // Do any additional setup after loading the view.
     }
 
@@ -31,15 +45,48 @@ class RegisterViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func Register(){
+        if (pass.text?.characters.count)! < 7 {
+            error.isHidden=false;
+            self.error.text="Password must be greater than 6 characters"
+            return
+        }
+        else {
+            error.isHidden=true;
+        }
+        
+        if pass.text != repass.text {
+            error.isHidden=false;
+            self.error.text="Repassword was wrong"
+            return
+        }
+        else{
+            error.isHidden=true;
+        }
+        
+        Auth.auth().createUser(withEmail: self.email.text!, password: self.pass.text!, completion: {
+            user,error in
+            if(error != nil){
+                self.error.isHidden=false;
+                self.error.text="Email's format was wrong or email existed"
+            }else{
+                Auth.auth().signIn(withEmail: self.email.text!, password: self.pass.text!, completion: {
+                    user, error in
+                    if(error != nil){
+                        print("error");
+                        return
+                    }
+                    else {
+                        let storyboard=UIStoryboard(name:"Play", bundle: nil)
+                        let vc=storyboard.instantiateViewController(withIdentifier: "playNavi") as UIViewController
+                        self.present(vc, animated: true, completion: nil)
+                        return
+                    }
+                })
+            }
+        })
+        
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
 
 }
