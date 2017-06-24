72174c8b9a52d74e709e4bd66fe3890bf440b2f7
TTLAM <lamtran2601@gmail.com>
2017-06-24 09:19:19 +0700

    reup dictionary

--- Translator/Dictionary.swift
+++ Translator/Dictionary.swift
@@ -12,6 +12,50 @@ class Dictionary {
     static var words: [String] = []
     static var offsets: [Int] = []
     static var lengths: [Int] = []
-    static var dict: FileHandle? = FileHandle(forReadingAtPath: Bundle.main.path(forResource: "anhviet109K", ofType: "txt")!)
+    static var file: FileHandle? = FileHandle(forReadingAtPath: Bundle.main.path(forResource: "anhviet109K", ofType: "txt")!)
 
+    init() {
+        
+        var raws: [String?] = []
+        do {
+            // read file index
+            if let path = Bundle.main.path(forResource: "index", ofType: "txt"){
+                let data = try String(contentsOfFile:path, encoding: String.Encoding.utf8)
+                raws = data.components(separatedBy: "\n")
+                //print(raws)
+            }
+            else {
+                print("file index not found")
+            }
+        } catch let err as NSError {
+            // do something with Error
+            print(err)
+        }
+        
+        for line in raws {
+            if (!(line?.isEmpty)!) {
+                let ar: [String] = (line?.components(separatedBy: "\t"))!
+                Dictionary.words.append(ar[0])
+                Dictionary.offsets.append(Int(ar[1])!)
+                Dictionary.lengths.append(Int(ar[2])!)
+            }
+        }
+        
+
+    }
+    
+    static func getMean(word: String) -> String {
+        if let dict = file {
+            if let idx = Dictionary.words.index(of: word) {
+                dict.seek(toFileOffset: UInt64(offsets[idx]))
+                let data = dict.readData(ofLength: lengths[idx])
+                
+                    if let mean = String(data: data, encoding: String.Encoding.utf8) as String! {
+                        return mean
+                    }
+                
+            }
+        }
+        return "Not found this word"
+    }
 }
